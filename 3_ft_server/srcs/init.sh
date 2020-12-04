2409ab840274cc5b7c9e3ea6daf209a4d272553e
Adrien <adrienrossignol1@gmail.com>
2020-12-04 22:09:33 +0100

    try to fix

--- 3_ft_server/srcs/init.sh
+++ 3_ft_server/srcs/init.sh
@@ -1,17 +1,18 @@
-#!bin/bash
-
-chown -R www-data /var/www/*
-chmod -R 755 /var/www/*
-
-ln -s /etc/nginx/sites-avialable/localhost /etc/nginx/sites-enabled/
-rm -fr /etc/nginx/sites-enabled/default
-
+#Certificat SSL
 mkdir /etc/nginx/ssl
 openssl req -newkey rsa:4096 -sha256 -days 365 -nodes \
 			-out /etc/nging/ssl/localhost.pem \
 			-keyout /etc/nginx/ssl/localhost.key \
 			-subj "/C=BE/ST=Brussels/L=Brussels/O=19Belgium/OU=cadet/CN=localhost"
 
+#NGINX
+mkdir var/www/localhost
+mv ./nging.conf etc/nginx/sites-available
+ln -s etc/nginx/sites-available/default etc/nginx/sites-enabled
+chown -R www-data /var/www/*
+chmod -R 755 /var/www/*
+
+#MYSQL
 echo "CREATE DATABASE wordpress;" \
 			| mysql -u root --skip-password
 echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user='root';" \
@@ -29,6 +30,8 @@ wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-lang
 tar xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz
 mv phpMyAdmin-4.9.0.1-all-languages var/www/localhost/phpmyadmin
 mv ./config.inc.php var/www/localhost/phpmyadmin
+chmod 660 /var/www/localhost/phpmyadmin/config.inc.php
+chown -R www-data:www-data /var/www/localhost/phpmyadmin
 
 #WORDPRESS
 wget https://wordpress.org/latest.tar.gz
